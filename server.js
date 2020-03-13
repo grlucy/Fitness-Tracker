db6c7453b63b04471f7385dcb662df171ca2f647
unknown <53538316+grlucy@users.noreply.github.com>
2020-03-13 12:51:48 -0400

    created model, server, html routes

--- server.js
+++ server.js
@@ -2,3 +2,29 @@
 const express = require("express");
 const mongojs = require("mongojs");
 const logger = require("morgan");
+
+// Set up Mongo DB
+const databaseUrl = "fitness";
+const collections = ["workout"];
+const db = mongojs(databaseUrl, collections);
+
+// Create express server
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Point to server routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
